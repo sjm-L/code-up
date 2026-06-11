cca01cecaec4c745997563edc007e35383f7c001
sjm-L <shower1300@gmail.com>
2026-06-11 14:40:03 +0900

    content: 1258번 풀이 기록 추가
    CodeUp 제출 시 클래스 이름은 Main으로 작성해야 하며, 안내 문구 없이 정답만 출력해야 한다는 내용을 정리했다.

--- src/P1258.java
+++ src/P1258.java
@@ -1,20 +1,20 @@
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
-public class P1258 {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n;
-        int sum = 0;
 
-        System.out.println("수를 입력 하시요 : ");
-        n = sc.nextInt();
+        int n = sc.nextInt();
+        int sum = 0;
 
         for (int i = 1; i <= n; i++) {
             sum = sum + i;
         }
 
-        System.out.println("1부터" + n + "까지 합은  : ");
         System.out.println(sum);
     }
 }
+
+
+
